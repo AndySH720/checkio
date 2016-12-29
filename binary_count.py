defcfc4ce96fabed0cbc686b2465a1198bd90741
AndySH720 <andysh720@gmail.com>
2016-12-29 11:04:56 +0000

    binary_count.py

---
+++ binary_count.py
@@ -0,0 +1,12 @@
+def count_units(number):
+	n = bin(number)
+	n = list(n)
+	n = [x for x in n if x == '1']
+	return len(n)
+if __name__ == '__main__':
+    	# These "asserts" using only for self-checking and not necessary for auto-testing
+	assert count_units(4) == 1
+	assert count_units(15) == 4
+	assert count_units(1) == 1
+	assert count_units(1022) == 9
+	print("Use 'Check' to earn sweet rewards!")
